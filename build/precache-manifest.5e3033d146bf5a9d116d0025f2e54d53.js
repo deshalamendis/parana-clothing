e5c8aebd8a124ee1a8f89cca8155b29b1865e7ed
Deshala Mendis <deshala@DeshalanoMacBook-Pro.local>
2019-11-21 22:07:23 +0530

    adding files for github pages

---
+++ build/precache-manifest.5e3033d146bf5a9d116d0025f2e54d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b457aa83a382aee83a94e77cd9b4a5dd",
+    "url": "/parana-clothing/index.html"
+  },
+  {
+    "revision": "4f462b08d1e89da9b9a0",
+    "url": "/parana-clothing/static/css/main.ce099472.chunk.css"
+  },
+  {
+    "revision": "653ed0c77f8c494a78e6",
+    "url": "/parana-clothing/static/js/2.7314ef2a.chunk.js"
+  },
+  {
+    "revision": "4f462b08d1e89da9b9a0",
+    "url": "/parana-clothing/static/js/main.c821eb93.chunk.js"
+  },
+  {
+    "revision": "51873109cecca5336cdf",
+    "url": "/parana-clothing/static/js/runtime-main.d0509081.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/parana-clothing/static/media/logo.ed28accb.svg"
+  }
+]);
